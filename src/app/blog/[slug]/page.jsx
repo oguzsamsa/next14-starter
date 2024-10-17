0c0182b8571c960fcc1e1c33fea05d01d9bf4a6d
moscowmule7 <oguzsamsa@gmail.com>
2024-10-17 13:39:47 +0300

    home, about, contact, blog pages created

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import styles from './singlePost.module.css'
+import Image from 'next/image'
 
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/about.png" alt='' fill className={styles.img}/>
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image src="/about.png" className={styles.avatar} alt='' width={50} height={50} />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Terry Jefferson</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum rem repellat obcaecati distinctio inventore, officiis fugiat eum totam voluptatum cupiditate enim numquam minima quis vitae deserunt tempora suscipit aliquid natus.
+        </div>
+      </div>
+    </div>
   )
 }
 
